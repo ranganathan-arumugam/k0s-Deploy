3c1ed54803dbd202e0fa4d2952d1c981710de8ce
ranganathan-arumugam <ranganathan.arumugam@syncfusion.com>
2024-05-16 23:54:18 +0530

    ingress file changes and deploy.sh optimization

--- deploy.sh
+++ deploy.sh
@@ -199,19 +199,24 @@ function show_bold_reports_graphic {
 
 # Function to install k0s
 function install_k0s {
-  say 4 "Installing k0s..."
-  command_exists k0s && say 2 "k0s is already installed." || { curl -sSLf https://get.k0s.sh | sudo K0S_VERSION=v1.28.7+k0s.0 sh; }
+  say() { echo "$1" >&2; }
+  say "Installing k0s..."
+  if command -v k0s &> /dev/null; then
+    say "k0s is already installed."
+  else
+    curl -sSLf https://get.k0s.sh | sudo K0S_VERSION=v1.28.4+k0s.0 sh
+  fi
 }
 
 # Function to start k0s cluster
 function start_k0s {
-  k0s kubectl get nodes &> /dev/null || {
-    say 4 "Starting k0s cluster..."
+  if ! k0s kubectl get nodes &> /dev/null; then
+    say "Starting k0s cluster..."
     sudo k0s install controller --single &
-    sleep 15
+    sleep 10
     sudo k0s start &
-    sleep 15
-  }
+    sleep 10
+  fi
 }
 
 # Function to install Bold Reports
