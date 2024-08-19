ae4020ccd8714f32cd5fa699a505bdd2caa78f66
ranganathan-arumugam <66311251+ranganathan-arumugam@users.noreply.github.com>
2024-08-19 10:32:23 +0530

    Update deploy.sh
    testing storage account changes

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ set -e
 # Variable declaration
 repo_url="https://github.com/ranganathan-arumugam/k0s-Deploy/raw/main/private-cloud.zip"
 destination="/manifest"
-storage_account_name="managedhostingacc"
-fileshare_name="managedhostingfs"
+storage_account_name="k0ssacc"
+fileshare_name="k0sfs"
 
 # Parse command-line arguments
 for arg in "$@"; do
